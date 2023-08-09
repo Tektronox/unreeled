ef236515136421788c913b98d58e998c525ce70e
Tim <tim.schelbert@gmail.com>
2023-08-09 19:03:53 +0200

    Yesss

--- background.js
+++ background.js
@@ -1,8 +1,7 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("https://www.instagram.com") &&
-        tab.url != "https://www.instagram.com/?variant=following") {
+    if (tab.url && tab.url.includes("https://www.instagram.com")) {
 
-        console.log('sending message');
+        console.log('sending message to tab, url: ' + tab.url);
         chrome.tabs.sendMessage(tabId, {
             type: "NEW",
         });
--- contentScript.js
+++ contentScript.js
@@ -9,22 +9,6 @@ const getXPathElement = (path) => {
     return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 };
     
-const getFollowingTab = () => {
-    return getXPathElement(followingPath);
-};
-
-const getTopBar = () => {
-    return getXPathElement(topBarPath);
-};
-
-const getReelsButton = () => {
-    return getXPathElement(reelsButtonPath);
-};
-
-const getExploreButton = () => {
-    return getXPathElement(exploreButtonPath);
-};
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     const { type } = request;
     if (type === 'NEW') {
@@ -35,27 +19,65 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 const removeStuff = () => {
     const url = window.location.href;
+    console.log('removeStuff called, url: ' + url);
+    
     if (url === 'https://www.instagram.com/') {
     window.location.href = 'https://www.instagram.com/?variant=following';
     }
+    else if (url === 'https://www.instagram.com/?variant=following') {
+        
+        // const followingTab = locateElementByXPath(followingPath, 1000);
+        // if (followingTab) {
+        //     //followingTab.click();
+        //     getTopBar().remove();
+        // }
+        // else {
+        //     console.log('followingTab not found');
+        // }
 
-    // click on the following tab if it exists and remove the for you / following bar
-    const followingTab = getFollowingTab();
-    if (followingTab) {
-        //followingTab.click();
-        getTopBar().remove();
-    }
+        locateWithXPath(topBarPath, 1000)
+            .then((topBarPath) => {
+                topBarPath.remove();
+            })
+            .catch((error) => {
+                console.log('followingTab not found', error);
+            });
 
-    const reelsButton = getReelsButton();
-    if (reelsButton) {
-        reelsButton.remove();
-    }
+        locateWithXPath(reelsButtonPath, 1000)
+            .then((reelsButton) => {
+                reelsButton.remove();
+            })
+            .catch((error) => {
+                console.log('reelsButton not found', error);
+            });
 
-    const exploreButton = getExploreButton();
-    if (exploreButton) {
-        exploreButton.remove();
+        locateWithXPath(exploreButtonPath, 1000)
+            .then((exploreButton) => {
+                exploreButton.remove();
+            })
+            .catch((error) => {
+                console.log('exploreButton not found', error);
+            });
     }
 };
+
+
+const locateWithXPath = (xpath, timeout) => {
+    const endTime = Date.now() + timeout;
+    const checkCondition = (resolve, reject) => {
+        const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        if (element) {
+            resolve(element);
+        }
+        else if (Date.now() < endTime) {
+            setTimeout(checkCondition, 100, resolve, reject);
+        }
+        else {
+            reject(new Error('Element not found with XPath: ' + xpath));
+        }
+    };
+    return new Promise(checkCondition);
+};    
 
 
 
