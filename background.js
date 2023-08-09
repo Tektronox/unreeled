f4fa81bbeba75bbbd4e4f6e36417cbfaf54eda75
Tim <tim.schelbert@gmail.com>
2023-08-09 19:23:36 +0200

    remove debug logs

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
     if (tab.url && tab.url.includes("https://www.instagram.com")) {
 
-        console.log('sending message to tab, url: ' + tab.url);
+        //console.log('sending message to tab, url: ' + tab.url);
         chrome.tabs.sendMessage(tabId, {
             type: "NEW",
         });
